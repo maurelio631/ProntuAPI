bdad3edf01ac003a404425cdc31feb7118a34484
Your Name <you@example.com>
2022-01-13 22:09:01 -0300

    ajustando a estrutura e removendo arquivos

--- database/database.js
+++
@@ -1,7 +0,0 @@
-import {MongoClient} from "mongodb"
-import { connect } from "mongoose"
-MongoClient.connect("mongodb://localhost")
-.then(conn=> global.conn = conn.db('Employee'))
-.catch(err => console.log(err))
-
-module.exports = {}
--- index.js
+++ index.js
@@ -3,24 +3,15 @@ import usersRoutes from "./routes/users.routes.js";
 import cors from "cors";
 import mongoose from "mongoose";
 import router from "./routes/users.routes.js";
-import dotenv from "dotenv";
-
-dotenv.config();
-
+import { MongoClient } from "mongodb";
 const app = express();
 
+
 app.use("/users", usersRoutes);
-app.use("/", (req,res)=>{
+app.use("/", (req, res) => {
     res.send("Seja bem vindo à api de cadastro de usuários prontu");
 });
 app.use(express.json({ extended: true, limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
-const CONNECTION_URL = process.env.CONNECTION_STRING;
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=>app.listen(PORT,()=>console.log(`Server running on port http://localhost:${PORT}`)))
-.catch((err)=>console.log(err.message));
 
